3b44bc5b712836fcb7aa4df9535b2b6d11c027d7
pedro <pedrosantosjovem2004@gmail.com>
2024-07-04 16:02:19 -0300

    oi

--- js/script.js
+++ js/script.js
@@ -89,6 +89,7 @@ function detalhes(){
     .then((res)=>res.json())
     .then((dados)=>{
         dados.payload.map((rs)=>{
+          document.querySelector("h2").innerHTML = "Detalhes do livro: " +rs.nometitulo
             let card = ` <div class="card mb-3" col-md-10">
             <div class="row g-0">
               <div class="col-md-4" >
@@ -120,6 +121,8 @@ function detalhes(){
                   <h5 class="card-title">${rs.autor}</h5>
                   <p class="card-text">${rs.sinopse}</p>
                   <p class="card-text precoatual">R$ ${rs.precodesconto < 1 ? rs.precoatual : rs.precodesconto}</p>
+                  <a href="carrinho.html?idlivro=${rs.idtitulo} class="carrinho"> 
+                  <img src=img/card.png widht=40 heigt=40"> incluir no carrinho</a>
                 </div>
               </div>
             </div>
@@ -131,3 +134,59 @@ function detalhes(){
     .catch((error)=>console.error(`erro na api ${error}`))
 }
 
+function buscar(){
+
+  const conteudo = document.querySelector(".conteudo");
+  conteudo.innerHTML = "";
+  let palavra = document.querySelector("input").value;
+  document.querySelector("h2").innerHTML = `Você pesquisou por: ${palavra}`
+  fetch("http://127.0.0.1:4001/api/v1/livros/detalhes/titulos/"+palavra)
+    .then((res)=>res.json())
+    .then((dados)=>{
+        dados.payload.map((rs)=>{
+            let card = ` <div class="card mb-3" col-md-10">
+            <div class="row g-0">
+              <div class="col-md-4" >
+              <div id="carouselExample" class="carousel slide">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="${rs.foto1}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="${rs.foto2}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="${rs.foto3}" class="d-block w-100" alt="...">
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h3 class="card-title">${rs.nometitulo}</h3>
+                  <h5 class="card-title">${rs.autor}</h5>
+                  <p class="card-text precoatual">R$ ${rs.precodesconto < 1 ? rs.precoatual : rs.precodesconto}</p>
+                  <div>
+                  <a class="btn btn-dark" href="detalhes.html?idlivro=${rs.idtitulos}">Saiba mais</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
+
+            conteudo.innerHTML += card;
+        })
+    })
+    .catch((error)=>console.error(`erro na api ${error}`))
+
+
+
+}

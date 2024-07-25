ba1ea40a0fee410f30f5554adbd9bc83b7e108b1
pedro <pedrosantosjovem2004@gmail.com>
2024-07-25 17:15:11 -0300

    atualizaçao

--- js/script.js
+++ js/script.js
@@ -1,96 +1,96 @@
-function login(){
-    const us = document.querySelector("#nomeusuario");
-    const sh = document.querySelector("#senha");
+function login() {
+  const us = document.querySelector("#nomeusuario");
+  const sh = document.querySelector("#senha");
 
-    // usamos o comando trim para elmiar os espaços
+  // usamos o comando trim para elmiar os espaços
 
-    if(us.value.trim() == "" || sh.value.trim()==""){
-        return alert("voce deve precher os campos")
-    }
+  if (us.value.trim() == "" || sh.value.trim() == "") {
+    return alert("voce deve precher os campos")
+  }
 
-    fetch("http://127.0.0.1:4000/api/v1/users/login",{
-        method:"POST",
-        headers:{
-            "accept":"application/json",
-            "content-type":"application/json"
-        },
-        body:JSON.stringify({
-            nomeusuario:us.value,
-            senha:sh.value
-        })
-    }).then((res)=>res.json())
-    .then((result)=>{
-        console.log(result)
+  fetch("http://127.0.0.1:4000/api/v1/users/login", {
+    method: "POST",
+    headers: {
+      "accept": "application/json",
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      nomeusuario: us.value,
+      senha: sh.value
     })
-    .catch((error)=>console.error(`erro ao tentar acessar a api ${error}`));
+  }).then((res) => res.json())
+    .then((result) => {
+      console.log(result)
+    })
+    .catch((error) => console.error(`erro ao tentar acessar a api ${error}`));
 
 
 
 }
 
-function cadastrarUsuario(){
-    const us = document.querySelector("#txtusuario");
-    const sh = document.querySelector("#txtsenha");
-    const ft = document.querySelector("#txtfotoperfil");
+function cadastrarUsuario() {
+  const us = document.querySelector("#txtusuario");
+  const sh = document.querySelector("#txtsenha");
+  const ft = document.querySelector("#txtfotoperfil");
 
-    if(us.value.trim()=="" || sh.value.trim()=="" || ft.value.trim()==""){
-        return alert("Prencha todos os campos")
-    }
-    fetch("http://127.0.0.1:4000/api/v1/users/cadastrar",{
-        method:"POST",
-        headers:{
-            "accept":"application/json",
-            "content-type":"application/json"
-        },
-        body:JSON.stringify({
-            nomeusuario:us.value,
-            senha:sh.value,
-            foto:ft.value
-        })
+  if (us.value.trim() == "" || sh.value.trim() == "" || ft.value.trim() == "") {
+    return alert("Prencha todos os campos")
+  }
+  fetch("http://127.0.0.1:4000/api/v1/users/cadastrar", {
+    method: "POST",
+    headers: {
+      "accept": "application/json",
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      nomeusuario: us.value,
+      senha: sh.value,
+      foto: ft.value
     })
-    .then((res)=>res.json())
-    .then((result)=>{
-        console.log(result);
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
     })
-    .catch((error)=>console.error(`erro na api ${error}`))
+    .catch((error) => console.error(`erro na api ${error}`))
 }
 
-function carregarLivros(){
-    const conteudo = document.querySelector(".conteudo");
-    fetch("http://127.0.0.1:4001/api/v1/livros/detalhes")
-    .then((res)=>res.json())
-    .then((dados)=>{
-        dados.payload.map((rs)=>{
-            let card = `<div class="card" style="width: 18rem;">
+function carregarLivros() {
+  const conteudo = document.querySelector(".conteudo");
+  fetch("http://127.0.0.1:4001/api/v1/livros/detalhes")
+    .then((res) => res.json())
+    .then((dados) => {
+      dados.payload.map((rs) => {
+        let card = `<div class="card" style="width: 18rem;">
             <img src=${rs.foto1} class="card-img-top" alt="...">
             <div class="card-body">
             <h3>${rs.nometitulo}</h3>
               <p class="card-text">Autor:${rs.autor} </p>
               <div class="containerflex">
               <p class="card-text">De R$ ${rs.precoatual} </p>
-              <p class="card-text"> Por R$ ${rs.precodesconto<1 ? rs.precoatual : rs.precodesconto}</p>
+              <p class="card-text"> Por R$ ${rs.precodesconto < 1 ? rs.precoatual : rs.precodesconto}</p>
               </div>
               <a class="btn btn-dark" href="detalhes.html?idlivro=${rs.idtitulos}">Saiba mais</a>
             </div>
           </div>`;
 
-            conteudo.innerHTML += card;
-        })
+        conteudo.innerHTML += card;
+      })
     })
-    .catch((error)=>console.error(`erro na api ${error}`))
-    
+    .catch((error) => console.error(`erro na api ${error}`))
+
 }
 
-function detalhes(){
-    let id_url = window.location.search.split('=');
-    const conteudo = document.querySelector(".conteudo");
+function detalhes() {
+  let id_url = window.location.search.split('=');
+  const conteudo = document.querySelector(".conteudo");
 
-    fetch("http://127.0.0.1:4001/api/v1/livros/detalhes/"+id_url[1])
-    .then((res)=>res.json())
-    .then((dados)=>{
-        dados.payload.map((rs)=>{
-          document.querySelector("h2").innerHTML = "Detalhes do livro: " +rs.nometitulo
-            let card = ` <div class="card mb-3" col-md-10">
+  fetch("http://127.0.0.1:4001/api/v1/livros/detalhes/" + id_url[1])
+    .then((res) => res.json())
+    .then((dados) => {
+      dados.payload.map((rs) => {
+        document.querySelector("h2").innerHTML = "Detalhes do livro: " + rs.nometitulo
+        let card = ` <div class="card mb-3" col-md-10">
             <div class="row g-0">
               <div class="col-md-4" >
               <div id="carouselExample" class="carousel slide">
@@ -129,23 +129,23 @@ function detalhes(){
             </div>
           </div>`;
 
-            conteudo.innerHTML += card;
-        })
+        conteudo.innerHTML += card;
+      })
     })
-    .catch((error)=>console.error(`erro na api ${error}`))
+    .catch((error) => console.error(`erro na api ${error}`))
 }
 
-function buscar(){
+function buscar() {
 
   const conteudo = document.querySelector(".conteudo");
   conteudo.innerHTML = "";
   let palavra = document.querySelector("input").value;
   document.querySelector("h2").innerHTML = `Você pesquisou por: ${palavra}`
-  fetch("http://127.0.0.1:4001/api/v1/livros/detalhes/titulos/"+palavra)
-    .then((res)=>res.json())
-    .then((dados)=>{
-        dados.payload.map((rs)=>{
-            let card = ` <div class="card mb-3" col-md-10">
+  fetch("http://127.0.0.1:4001/api/v1/livros/detalhes/titulos/" + palavra)
+    .then((res) => res.json())
+    .then((dados) => {
+      dados.payload.map((rs) => {
+        let card = ` <div class="card mb-3" col-md-10">
             <div class="row g-0">
               <div class="col-md-4" >
               <div id="carouselExample" class="carousel slide">
@@ -183,39 +183,68 @@ function buscar(){
             </div>
           </div>`;
 
-            conteudo.innerHTML += card;
-        })
+        conteudo.innerHTML += card;
+      })
     })
-    .catch((error)=>console.error(`erro na api ${error}`))
+    .catch((error) => console.error(`erro na api ${error}`))
 
 
 
 }
 
-function carregarCarrinho(){
-  let id_url = window.location.search.split('=');
-    const conteudo = document.querySelector(".conteudo");
-
-    fetch("http://127.0.0.1:4002/api/v1/carrinho/listar"+id_url[1])
-    .then((res)=>res.json())
-    .then((dados)=>{
-        dados.payload.map((rs)=>{
-          document.querySelector("h2").innerHTML = "Detalhes do livro: " +rs.nometitulo
-          let card = `<div class="card" style="width: 18rem;">
-          <img src=${rs.foto1} class="card-img-top" alt="...">
-          <div class="card-body">
-          <h3>${rs.nometitulo}</h3>
-            <p class="card-text">Autor:${rs.autor} </p>
-            <div class="containerflex">
-            <p class="card-text">De R$ ${rs.precoatual} </p>
-            <p class="card-text"> Por R$ ${rs.precodesconto<1 ? rs.precoatual : rs.precodesconto}</p>
-            </div>
-            <a class="btn btn-dark" href="detalhes.html?idlivro=${rs.idtitulos}">Saiba mais</a>
-          </div>
-        </div>`;
-
-            conteudo.innerHTML += card;
-        })
+function carregarCarrinho() {
+  const conteudo = document.querySelector('.conteudo')
+  fetch("http://127.0.0.1:4002/api/v1/carrinho/listar/" + 1)
+    .then((res) => res.json())
+    .then((dados) => {
+      dados.payload.map((rs) => {
+        let card = `<div class="card mb-3 col-md-7">
+                <div class="row g-0">
+                  <div class="col-md-3">
+                  <img src=${rs.foto1} class="card-img-top" alt="...">
+                </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h3 class="card-title">${rs.nometitulo}</h3>
+                      <h6 class="card-title">Autor: ${rs.autor}</h6>
+                      <p class="card-text precoatual"" >${rs.precoatual}</p>
+                      <select class="form-select form-select-lg mb-3" aria-label="Large select example" style="width:70px">
+                      <option value="1">${rs.quantidade}</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      </select>
+                      <p class="card-text" >${rs.total}</p>
+                      </div>
+                  </div>
+                </div>
+              </div>`
+        conteudo.innerHTML += card
+      })
     })
-    .catch((error)=>console.error(`erro na api ${error}`))
+    .catch((error) => {
+      console.error(`Erro na api ${error}`)
+    })
+
+    somarValor()
+}
+
+function somarValor() {
+  const conteudo = document.querySelector('.subtotal')
+
+  fetch("http://127.0.0.1:4002/api/v1/carrinho/somar/" + 1)
+    .then((res) => res.json())
+    .then((dados) => {
+      dados.payload.map((rs) => {
+        let cardSubTotal = `<div class="card text-center mb-3" style="width: 18rem;">
+        <div class="card-body">
+          <p class="card-text">Subtotal: R$ ${rs.subtotal}</p>
+          <a href="#" class="btn btn-primary">Fechar pedido</a>
+        </div>
+      </div>`
+        conteudo.innerHTML = cardSubTotal
+      })
+    })
+    .catch((error) => {
+      console.error(`Erro na api ${error}`)
+    })
 }
